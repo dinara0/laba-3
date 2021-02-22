5a71ed315ce317f03243c9f610d973e0d4f7be46
Динара с <dinartas1@gmail.com>
2021-02-22 21:01:14 +0500

    1

--- 1/1.cpp
+++ 1/1.cpp
@@ -8,8 +8,27 @@ public:
 	virtual void set_value(int i, int value) = NULL;
 	virtual int get_value(int i) = NULL;
 	virtual int delete_value(int i) = NULL;
+	virtual void prev() = NULL;
+	virtual void next() = NULL;
 };
-/*
+class Array : public Iarray {
+private:
+	int data[N];
+	int curr;
+public:
+
+	virtual void set_value(int i, int value) = NULL;
+	virtual int get_value(int i) {
+		return
+	}
+
+
+	virtual int delete_value(int i) = NULL;
+	virtual void prev() = NULL;
+	virtual void next() = NULL;
+};
+};
+	/*
 class Circle : public Figure {
 	void Message() {
 		printf("Circle\n");
